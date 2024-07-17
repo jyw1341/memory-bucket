80ed933f02293311f91126afc995ca8e890fcffe
정용우 <jyw1341@gmail.com>
2024-07-17 22:52:50 +0900

    refactor: 안쓰는 클래스 삭제

--- api/src/main/java/com/zephyr/api/repository/MemoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zephyr.api.repository;
-
-import com.zephyr.api.domain.Memory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemoryRepository extends JpaRepository<Memory, Long> {
-}
